859488e4e3b6ea51224a31730638e63c01e0ae7c
Jeffrey Gillis <jeffrey.gillis1@gmail.com>
2019-05-05 09:52:46 -0400

    Update data reference

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app" class="col-12">
-    <input type="text" class="col-12" v-model="search" placeholder="Date or Author or Title Search"/>
+    <input type="text" class="col-12" v-model="searchString" placeholder="Date or Author or Title Search"/>
     <ul class="col-12">
       <li class="col-12" v-for="recording in filteredRecordings">
         <a :href="recording.htmlPath">{{recording.date}} - {{recording.author}} - {{recording.title}}</a>
@@ -15,18 +15,18 @@ export default {
   data () {
     return {
       recordings: [],
-      search: ''
+      searchString: ''
     }
   },
   created () {
     this.$http.get('/recordings.json').then(function (data) {
-      this.recordings = data.body.reverse()
+      this.recordings = data.body
     })
   },
   computed: {
     filteredRecordings: function () {
       return this.recordings.filter((recording) => {
-        return recording.title.match(this.search) || recording.author.match(this.search) || recording.date.match(this.search)
+        return recording.title.match(this.searchString) || recording.author.match(this.searchString) || recording.date.match(this.searchString)
       })
     }
   }
